60251727045a340f077bbdaa0198bb8c68e82c46
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-06 01:44:51 +0100

    tweaked filter functionality (don't merge into public yet)

--- assets/js/event-filter.js
+++ assets/js/event-filter.js
@@ -1,14 +1,16 @@
 // Filter code
 const itemFilter = document.getElementById('filter');
-const events = document.getElementById('events');
+const eventContainer = document.getElementById('events');
 
-// Promise
+// Promises
+let events = [];
 let names = [];
-getNames().then(response => names = [...response]);
 
-function getEventNames() {
+
+
+function getEvents() {
   return new Promise((resolve) => {
-    const eventNames = events.querySelectorAll('.card-title');
+    const eventNames = eventContainer.querySelectorAll('.card-title');
     setTimeout(() => { 
       resolve(eventNames);
     }, 10);
@@ -18,20 +20,39 @@ function getEventNames() {
 function getNames() {
   console.log('Getting Names');
   return new Promise (resolve => {
-    getEventNames()
+    getEvents()
     .then( (response) => {
       console.log('events retrieved' + response)
-      const array = Array(...response).map(event => event.innerText.toLowerCase());
+      const filterArray = Array(...response).filter(item => item.parentElement.classList.contains('row') === false);
+      const array = filterArray.map(event => event.innerText.toLowerCase());
       console.log(array);
       resolve(array);
     })
   })
 }
 
-function setEventClass(event) {
+function getEventTitles() {
+  console.log('Getting Event Title elements');
+  return new Promise (resolve => {
+    getEvents()
+    .then( (response) => {
+      console.log('events retrieved' + response)
+      const filterArray = Array(...response).filter(item => item.parentElement.classList.contains('row') === false);
+      const array = filterArray.map(event => event.innerText.toLowerCase());
+      console.log(array);
+      resolve(array);
+    })
+  })
+}
+
+function updateEventClass(event, removeClass) {
   // find the event in the event
-  // set the class
-  // 
+  console.log('setting event class - ', event.textContent);
+  if (removeClass){
+    event.parentNode.parentNode.parentNode.parentNode.classList.remove('hidden');
+  } else {
+    event.parentNode.parentNode.parentNode.parentNode.classList.add('hidden');
+  }
 }
 
 
@@ -39,17 +60,22 @@ async function filterEvents(e) {
   // get the text input from the filter
   const text = e.target.value.toLowerCase();
 
-
-  if (Array.isArray(names) ){
+  if (names.length !== 0 || events.length !== 0){
     names.forEach(name => {
       // filters based on itemName 
       //- '-1' means the total of chars is not in there
       if (name.indexOf(text) != -1 ) {
-        console.log('contains filter letters');
-        // find parent and add 'hidden' class
+        console.log('contains filter letters, show element');
+        console.log(name);
+
+        const event = events.filter( item => item.innerText.toLowerCase() === name)[0];
+        console.log(event);
+        updateEventClass(event, true);
       } else {
-        console.log('does not contain filter letter');
-        // find parent and remove 'hidden' class
+        console.log('does not contain filter letter, hide element');
+        const event = events.filter( item => item.innerText.toLowerCase() === name)[0];
+        console.log(event);
+        updateEventClass(event);
       }
     });
   } else {
@@ -57,9 +83,24 @@ async function filterEvents(e) {
   }
 }
 
+async function setup() {
+  console.log('setup');
+  // get events
+  events = Array(...eventContainer.querySelectorAll('.card-title'));
+  events = events.filter(event => event.parentElement.classList.contains('row') === false);
+  console.log(events, 'events');
+
+  names = await getNames();
+
+  // get names from events
+  console.log(names, 'names');
+
+  console.log('setup completed');
+}
+
 
 
 // Attach Listeners
-
-
 itemFilter.addEventListener('input', filterEvents);
+
+setup();
