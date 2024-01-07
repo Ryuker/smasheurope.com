937ba3b7563780fe9438975d135bf94d7b75ad4e
ryuker <lifeisagamenotaproduct@gmail.com>
2024-01-07 13:33:59 +0100

    experimenting with new event-filter (don't merge into public)

---
+++ assets/js/filter/event-filter-new.js
@@ -0,0 +1,19 @@
+// New Optimized Event Filter 
+const eventsEls = document.querySelectorAll('#events .card-title');
+
+// const itemFilter = document.getElementById('filter');
+
+// names.forEach(event => event.textContent);
+
+// const events = await document.querySelectorAll('#events .card-title');
+// console.dir(names);
+
+document.addEventListener("DOMContentLoaded", function(){
+  // Code here waits to run until the DOM is loaded.
+  const eventsEls = document.querySelectorAll('#events .card-title');
+  // console.dir(names.forEach(event => event.textContent), 'names');
+});
+
+function setup(){
+  const eventsEls = document.querySelectorAll('#events .card-title');
+}
