3651681c3e5de30d413bd234defacd6254a88b37
ryuker <lifeisagamenotaproduct@gmail.com>
2025-01-28 13:05:05 +0100

    modified to build to dist folder instead of dist/docs, tweaked cname

--- new-version/astro.config.mjs
+++ new-version/astro.config.mjs
@@ -25,7 +25,7 @@ export default defineConfig({
   // output: 'server',
   output: 'static',
   site: 'https://ryuker.github.io/',
-  outDir: './dist/docs/',
+  outDir: './dist/',
   build: {
     assets: 'astro',
   },
